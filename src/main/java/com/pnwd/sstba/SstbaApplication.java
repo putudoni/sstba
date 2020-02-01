738bbc92cfd28fc6ac0a07e86a275a213be9f860
putudoni <putuwigadoni@yahoo.com>
2020-02-01 21:45:44 +0700

    Add condition whether collection size empty or not empty and remove dropCollection

--- src/main/java/com/pnwd/sstba/SstbaApplication.java
+++ src/main/java/com/pnwd/sstba/SstbaApplication.java
@@ -21,13 +21,14 @@ public class SstbaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		mongoTemplate.dropCollection("users");
-		User user = new User();
-		user.setUsername("admin");
-		user.setPassword("e38ad214943daad1d64c102faec29de4afe9da3d");
-		user.setToken("e4d437bb37fedc4183ee3f658b13c6633593a4a8");
-		userService.save(user);
-		System.out.println("API_KEY = "+user.getToken());
+		if(mongoTemplate.findAll(User.class).size() == 0) {
+			User user = new User();
+			user.setUsername("admin");
+			user.setPassword("e38ad214943daad1d64c102faec29de4afe9da3d");
+			user.setToken("e4d437bb37fedc4183ee3f658b13c6633593a4a8");
+			userService.save(user);
+			System.out.println("API_KEY = "+user.getToken());
+		}
 	}
 
 }
