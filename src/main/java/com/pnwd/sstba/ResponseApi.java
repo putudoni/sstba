ffb8e22d40899a6df61a49533bd64a797e66cf4d
putudoni <putuwigadoni@yahoo.com>
2020-02-02 14:20:28 +0700

    Format element

--- src/main/java/com/pnwd/sstba/ResponseApi.java
+++ src/main/java/com/pnwd/sstba/ResponseApi.java
@@ -10,25 +10,33 @@ package com.pnwd.sstba;
 public class ResponseApi {
 
 	private String status;
+
 	private String data;
+
 	private String messages;
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public String getData() {
 		return data;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
+
 	public String getMessages() {
 		return messages;
 	}
+
 	public void setMessages(String messages) {
 		this.messages = messages;
 	}
-	
+
 }
