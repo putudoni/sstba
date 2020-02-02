a258da8906279ec6fe237e5a539d8adc623dafe6
putudoni <putuwigadoni@yahoo.com>
2020-02-02 22:26:43 +0700

    Create LoginApi Class

---
+++ src/main/java/com/pnwd/sstba/LoginApi.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.pnwd.sstba;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+@RestController
+public class LoginApi {
+
+}
