9903b30a0dafc43e3868a55c34fc5ed3c6d79b67
putudoni <putuwigadoni@yahoo.com>
2020-02-02 22:39:32 +0700

    Create login method

--- src/main/java/com/pnwd/sstba/LoginApi.java
+++ src/main/java/com/pnwd/sstba/LoginApi.java
@@ -3,6 +3,18 @@
  */
 package com.pnwd.sstba;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginApi {
 
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@PostMapping("/login")
+	public ResponseEntity<ResponseApi> login(@Valid @RequestBody User user){
+		ResponseApi response = new ResponseApi();
+		Optional<User> users = userService.findByUsername(user.getUsername());
+		if(users.isPresent()) {
+			if(passwordEncoder.matches(user.getPassword(), users.get().getPassword())) {
+				Map<String, Object> data = new HashMap<>();
+				data.put("token", users.get().getToken());
+				response.setData(data);
+				response.setMessages("Welcome "+users.get().getUsername());
+			}else {
+				response.setMessages("Invalid password");
+			}
+		}else {
+			response.setMessages("User not found");
+		}
+		
+		response.setStatus(HttpStatus.OK.toString());
+		return new ResponseEntity<ResponseApi>(response,HttpStatus.OK);
+	}
+	
 }
